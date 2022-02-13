91916650305f79517e0fdc9ba5afa1e8d7913a4d
BH <74618590+harringt23@users.noreply.github.com>
2022-02-13 14:58:25 -0800

    asdf

--- hw2/ext2shaLookup_v2.c
+++ hw2/ext2shaLookup_v2.c
@@ -2,16 +2,41 @@
 #include <stdlib.h>
 #include <openssl/sha.h>
 
-/* ext2shaLookup_v2.c 
+/* ext2shaLookup_v2.c
  * Brynn Harrington
- * 
- * Will be identical to v1, but the read, lookup, compare will use byte based 
- * operations and memory compares. This time, you will read 41bytes (which is 
- * 40bytes hash + 1 character that is ‘\n’. The fscanf in the version 1 of your 
+ *
+ * Will be identical to v1, but the read, lookup, compare will use byte based
+ * operations and memory compares. This time, you will read 41bytes (which is
+ * 40bytes hash + 1 character that is ‘\n’. The fscanf in the version 1 of your
  * solution will consume the \n and that’s how fscanf knows that it hit the end
- * of the line. fread(2) does not do that as it reads raw un-buffered input and 
- * it will read exactly the number of bytes you want it to read. Be careful and 
- * troubleshoot your program behavior on the lookup of a known hash in a file 
- * (Hint: make sure when you are comparing the hashes that you only compare the 
- * hash itself and no ‘additional’ characters. 
+ * of the line. fread(2) does not do that as it reads raw un-buffered input and
+ * it will read exactly the number of bytes you want it to read. Be careful and
+ * troubleshoot your program behavior on the lookup of a known hash in a file
+ * (Hint: make sure when you are comparing the hashes that you only compare the
+ * hash itself and no ‘additional’ characters.
+ *
+ * Compile with: gcc -o ext2shaLookup_v2 ext2shaLookup_v2.c -lcrypto
+ * Execute with: ./ext2shaLookup_v2 <input hash file> <password string>
+ *
+ * Sample output:
+    $./text2shaLookup_v1 pwned_hashes.txt default
+    Searching for password “default”:
+    Searching in file “pwned_hashes.txt”
+    sha hash: 7505D64A54E061B7ACD54CCD58B49DC43500B635 Found, line: 7844
+    Time searching: 0.000960s
  */
+int main(const int argc, const char *argv[])
+{
+   // verify the user has the correct input parameters
+   if (argv != 2)
+   {
+      fprintf(stderr, "USAGE: ./ext2shaLookup_v2 <input hash file> <password string>\n");
+      return EXIT_FAILURE;
+   }
+
+   // byte_op_mem_comp();
+
+   return EXIT_SUCCESS;
+}
+
+return EXIT_SUCCESS;
--- hw2/text2shaLookup_v1.c
+++ hw2/text2shaLookup_v1.c
@@ -1,15 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <openssl/sha.h> 
+#include <openssl/sha.h>
 
-/* text2shaLookup_v1.c 
+/* text2shaLookup_v1.c
  * Brynn Harrington
- * 
  *
- * Use user level file operations primitives including: 
- * fopen, fscanf, fprintf, strlen, EOF, strcmp, strncmp... 
- * to read, lookup, and compare the hashed string passed in from CLI against the 
- * password hashes read from stored entries in the file. Read one line from the 
- * opened file and compare the two hash strings, and repeat until you check all 
- * file entries or you find a match .
+ *
+ * Use user level file operations primitives including:
+ * fopen, fscanf, fprintf, strlen, EOF, strcmp, strncmp...
+ * to read, lookup, and compare the hashed string passed in from CLI against the
+ * password hashes read from stored entries in the file. Read one line from the
+ * opened file and compare the two hash strings, and repeat until you check all
+ * file entries or you find a match.
+ *
+ * Compile with: gcc -o text2shaLookup_v1 text2shaLookup_v1.c -lcrypto
+ * Execute with: ./text2shaLookup_v1 <input hash file> <password string>
+ *
+ * Sample output:
+    $./text2shaLookup_v1 pwned_hashes.txt default
+    Searching for password “default”:
+    Searching in file “pwned_hashes.txt”
+    sha hash: 7505D64A54E061B7ACD54CCD58B49DC43500B635 Found, line: 7844
+    Time searching: 0.000960s
  */
+
+int main(const int argc, const char *argv[])
+{
+    // verify the user has the correct input parameters
+    if (argv != 2)
+    {
+        fprintf(stderr, "USAGE: ./text2shaLookup_v1 <input hash file> <password string>\n");
+        return EXIT_FAILURE;
+    }
+
+    // file_operation_primitives();
+
+    return EXIT_SUCCESS;
+}
